251a0dcb64d3d71c0e3e732a3ce8052fa22c569c
Vladimir Starkov <matmuchrapna@gmail.com>
2015-04-27 04:37:49 +0200

    final result

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-import Twitter from 'twitter';
+import tokens        from './tokens.json';
+import getAllTweets   from './get-twitter-all-tweets';
 
-import Promise from 'bluebird';
-import { info as log } from 'npmlog';
-import keys    from './keys.json';
+const options = { screen_name: 'largescalejs_ru', trim_user: true, count: 20 };
 
-const last = (arr)=> arr[arr.length - 1];
-
-const client = Promise.promisifyAll(new Twitter(keys));
-const getInfo = (options)=> client.getAsync('/users/show.json', options);
-const getTimeline = (options) => {
-  const defaultOptions = { include_rts: true, trim_user: true, count: 200 };
-  const resultOptions = Object.assign(defaultOptions, options);
-  return client.getAsync('/statuses/user_timeline.json', resultOptions);
-};
-
-// Timelines
-var count = 0;
-var items = [];
-getInfo({ screen_name: 'iamstarkov' }).then((res)=> {
-  count = res[0].statuses_count;
-  log(`Tweets count: ${count}`);
-
-  const setTimeline = (res)=> {
-    items = items.concat(res[0]);
-    const lastOne = last(items);
-
-    log(`${items.length} / ${lastOne.created_at}`);
-    if (items.length < count && items.length < 3200) {
-      getTimeline({ screen_name: 'iamstarkov', max_id: lastOne.id_str }).then(setTimeline);
-    }
-  }
-
-  getTimeline({ screen_name: 'iamstarkov' }).then(setTimeline);
+getAllTweets(tokens, options).then((tweets)=> {
+  console.log(`
+    count: ${tweets.length}
+    first: ${tweets[0].text}
+    last:  ${tweets[tweets.length - 1].text}
+  `);
 });
