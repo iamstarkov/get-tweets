d967a726dcaf818ae3024680a248f343d28b0e89
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-14 03:26:27 +0200

    internal refactoring

--- get-twitter-info.js
+++ get-twitter-info.js
@@ -1,7 +1,7 @@
 import Twitter from 'twitter';
 import Promise from 'bluebird';
 
-export default (tokens, apiOptions)=> {
+export default (tokens, options)=> {
   const client = Promise.promisifyAll(new Twitter(tokens));
-  return client.getAsync('/users/show.json', apiOptions);
+  return client.getAsync('/users/show.json', options);
 };
--- get-twitter-timeline.js
+++ get-twitter-timeline.js
@@ -1,7 +1,7 @@
 import Twitter from 'twitter';
 import Promise from 'bluebird';
 
-export default (tokens, apiOptions)=> {
+export default (tokens, options)=> {
   const client = Promise.promisifyAll(new Twitter(tokens));
-  return client.getAsync('/statuses/user_timeline.json', apiOptions);
+  return client.getAsync('/statuses/user_timeline.json', options);
 };
