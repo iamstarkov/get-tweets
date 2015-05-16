b0f89a2d132f1f7bf6681b1a722caa81350dccf8
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-16 22:11:00 +0200

    switch to new api in test

--- test.js
+++ test.js
@@ -1,26 +1,27 @@
 import { equal } from 'assert';
 import tokens    from './tokens.json';
 
-import getInfo       from './get-twitter-info';
-import getTimeline   from './get-twitter-timeline';
+import api           from './api';
 import getAllTweets  from './get-twitter-all-tweets';
 
 it('should fetch tweets count', (done)=> {
-  getInfo(tokens, { screen_name: 'largescalejs_ru' }).then((info)=> {
+  api.usersShow(tokens, { screen_name: 'largescalejs_ru' }).then((info)=> {
     equal(info.statuses_count, 37);
     done();
   });
 });
 
-it('should fetch all tweets in small account < 200 tweets', (done)=> {
-  getAllTweets(tokens, 'largescalejs_ru').then((tweets)=> {
-    equal(tweets.items.length + tweets.missed, 37);
-    done();
+it('should fetch all 37 tweets for @largescalejs_ru', (done)=> {
+  api.usersShow(tokens, { screen_name: 'largescalejs_ru' }).then((info)=> {
+    getAllTweets(tokens, 'largescalejs_ru').then((tweets)=> {
+      equal(tweets.items.length + tweets.missed, info.statuses_count);
+      done();
+    });
   });
 });
 
 it('should fetch all 1600+ tweets for @andrestaltz', (done)=> {
-  getInfo(tokens, { screen_name: 'andrestaltz' }).then((info)=> {
+  api.usersShow(tokens, { screen_name: 'andrestaltz' }).then((info)=> {
     getAllTweets(tokens, 'andrestaltz').then((tweets)=> {
       equal(tweets.items.length + tweets.missed, info.statuses_count);
       done();
@@ -29,7 +30,7 @@ it('should fetch all 1600+ tweets for @andrestaltz', (done)=> {
 });
 
 it('should fetch all 1900+ tweets for @Rygu', (done)=> {
-  getInfo(tokens, { screen_name: 'Rygu' }).then((info)=> {
+  api.usersShow(tokens, { screen_name: 'Rygu' }).then((info)=> {
     getAllTweets(tokens, 'Rygu').then((tweets)=> {
       equal(tweets.items.length + tweets.missed, info.statuses_count);
       done();
@@ -38,7 +39,7 @@ it('should fetch all 1900+ tweets for @Rygu', (done)=> {
 });
 
 it('should fetch all 2400+ tweets for @jsunderhood', (done)=> {
-  getInfo(tokens, { screen_name: 'jsunderhood' }).then((info)=> {
+  api.usersShow(tokens, { screen_name: 'jsunderhood' }).then((info)=> {
     getAllTweets(tokens, 'jsunderhood').then((tweets)=> {
       equal(tweets.items.length + tweets.missed, info.statuses_count);
       done();
