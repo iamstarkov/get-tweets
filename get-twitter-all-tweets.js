245f5ce00dbddc60ca409adf1fbe4dc2b6fbd29b
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-16 22:57:57 +0200

    extract setTimeline to pure function

--- get-twitter-all-tweets.js
+++ get-twitter-all-tweets.js
@@ -8,22 +8,26 @@ const getMaxId = (items)=> bignum(last(items).id_str).sub('1').toString(10);
 const _options = { trim_user: false, count: 200, include_rts: true, exclude_replies: false };
 const assign = Object.assign.bind(Object);
 
+const setTimeline = (info, storage, options, resolved, timeline)=> {
+  storage.items = storage.items.concat(timeline);
+  if (storage.items.length + storage.missed === info.statuses_count) {
+    return resolved(storage);
+  }
+  console.log(`${storage.items.length + storage.missed}`);
+  storage.missed += options.count - timeline.length;
+  return api.statusesUserTimeline(tokens, assign(options, {
+    max_id: getMaxId(storage.items)
+  })).then(setTimeline.bind(this, info, storage, options, resolved));
+};
+
 export default(tokens, screen_name)=> {
   const options = assign({screen_name}, _options);
-  var storage = { items: [], missed: 0 };
+  const storage = { items: [], missed: 0 };
   return api.usersShow(tokens, options).then((info)=> {
     console.log(`## @${info.screen_name}, ${info.statuses_count}`);
     return new Promise((resolved, reject)=> {
-      const setTimeline = (timeline)=> {
-        storage.items = storage.items.concat(timeline);
-        if (storage.items.length + storage.missed === info.statuses_count) {
-          return resolved(storage);
-        }
-        console.log(`${storage.items.length + storage.missed}`);
-        storage.missed += options.count - timeline.length;
-        return api.statusesUserTimeline(tokens, assign(options, { max_id: getMaxId(storage.items) })).then(setTimeline);
-      };
-      return api.statusesUserTimeline(tokens, options).then(setTimeline);
+      return api.statusesUserTimeline(tokens, options)
+        .then(setTimeline.bind(this, info, storage, options, resolved));
     });
   });
 };
