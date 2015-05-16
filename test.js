6b20f768ea9a7659b762bf3e217629939c1f0119
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-16 19:21:42 +0200

    final tests

--- test.js
+++ test.js
@@ -21,42 +21,35 @@ it('should fetch all tweets in small account < 200 tweets', (done)=> {
   });
 });
 
-it('should fetch all 1601 tweets for @andrestaltz', (done)=> {
-  const options = { screen_name: 'andrestaltz', trim_user: true, count: 200, include_rts: 1, exclude_replies: 0 };
-  getAllTweets(tokens, options).then((tweets)=> {
-    equal(tweets.list.length + tweets.missed, 1601);
-    done();
+it('should fetch all 1600+ tweets for @andrestaltz', (done)=> {
+  getInfo(tokens, { screen_name: 'andrestaltz' }).then((res) => {
+    var info = res[0];
+    const options = { screen_name: 'andrestaltz', trim_user: true, count: 200, include_rts: 1, exclude_replies: 0 };
+    getAllTweets(tokens, options).then((tweets)=> {
+      equal(tweets.list.length + tweets.missed, info.statuses_count);
+      done();
+    });
   });
 });
 
-it('should fetch all 1946 tweets for @Rygu', (done)=> {
-  const options = {
-    screen_name: 'Rygu',
-    trim_user: false,
-    count: 200,
-    include_rts: true,
-    exclude_replies: false,
-    contributor_details: true
-  };
-  getAllTweets(tokens, options).then((tweets)=> {
-    equal(tweets.list.length + tweets.missed, 1946);
-    equal(tweets.missed, 15);
-    done();
+it('should fetch all 1900+ tweets for @Rygu', (done)=> {
+  getInfo(tokens, { screen_name: 'Rygu' }).then((res) => {
+    var info = res[0];
+    const options = { screen_name: 'Rygu', trim_user: false, count: 200, include_rts: true, exclude_replies: false, contributor_details: true };
+    getAllTweets(tokens, options).then((tweets)=> {
+      equal(tweets.list.length + tweets.missed, info.statuses_count);
+      done();
+    });
   });
 });
 
-it('should fetch all 2397 tweets for @jsunderhood', (done)=> {
-  const options = { screen_name: 'jsunderhood', trim_user: true, count: 200, include_rts: 1, exclude_replies: 0 };
-  getAllTweets(tokens, options).then((tweets)=> {
-    equal(tweets.list.length + tweets.missed, 2397);
-    done();
-  });
-});
-
-it('should fetch all 758 tweets for @jhusain', (done)=> {
-  const options = { screen_name: 'jhusain', trim_user: true, count: 200, include_rts: 1, exclude_replies: 0 };
-  getAllTweets(tokens, options).then((tweets)=> {
-    equal(tweets.list.length + tweets.missed, 758);
-    done();
+it('should fetch all 2400+ tweets for @jsunderhood', (done)=> {
+  getInfo(tokens, { screen_name: 'jsunderhood' }).then((res) => {
+    var info = res[0];
+    const options = { screen_name: 'jsunderhood', trim_user: true, count: 200, include_rts: 1, exclude_replies: 0 };
+    getAllTweets(tokens, options).then((tweets)=> {
+      equal(tweets.list.length + tweets.missed, info.statuses_count);
+      done();
+    });
   });
 });
