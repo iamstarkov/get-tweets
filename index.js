f63377424fed499742e3200e91f7b5ee47331c0b
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-09-23 19:31:49 +0200

    wip remove all-tweets feature

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-import Twitter  from 'twitter';
-import bignum   from 'bn.js';
-import assign   from 'object-assign';
+import Twitter from 'twitter';
+import bignum  from 'bn.js';
+import { last, concat, contains, propEq, slice, findIndex } from 'ramda';
 
-const last = (arr)=> arr[arr.length - 1];
-const getMaxId = (items)=> (new bignum(last(items).id_str).sub(new bignum('1'))).toString();
+function accumulate(client, options, target, cb, tweets) {
+  const isTarget = propEq('id_str', target);
+  client.get('/statuses/user_timeline.json', options, (err, res) => {
+    if (err) throw new err;
+    if (res.length === 0) {
+      return cb(new Error('Target tweet is too far a way'));
+    }
+    tweets = concat(tweets, res);
+    if (contains(isTarget, tweets)) {
+      return cb(null, slice(0, findIndex(isTarget, tweets) + 1, tweets));
+    }
+    accumulate(client, options, target, cb, tweets);
+  });
+}
+
+export default function getTweets(tokens, screen_name, target, cb) {
+  const client = new Twitter(tokens);
+  const options = {
+    screen_name,
+    trim_user: true,
+    count: 200,
+    include_rts: true,
+    exclude_replies: false
+  };
+  accumulate(client, options, target, cb, []);
+};
+
+/*
+const getMaxId = (items) => (new bignum(last(items).id_str).sub(new bignum('1'))).toString();
 const _options = { trim_user: true, count: 200, include_rts: true, exclude_replies: false };
 
 const setTimeline = (client, resolve, target, info, items, missed, options, timeline)=> {
+  client.get('/statuses/user_timeline.json', options, (err, res, raw)=> {
+    if (err) throw new Error(`Code ${err[0].code}. ${err[0].message}`);
+    setTimeline(client, resolve, target, info, [], 0, options, res);
+  });
+
   items = items.concat(timeline);
 
-  if (!target && (items.length + missed === info.statuses_count)) {
-    return resolve(undefined, items, missed, info);
-  }
+
+  // if (!target && (items.length + missed === info.statuses_count)) {
+    // return resolve(undefined, items, missed, info);
+  // }
 
   if (target && items.find(i => i.id_str === target)) {
     const number = items.findIndex(i => i.id_str === target) + 1;
@@ -22,30 +55,22 @@ const setTimeline = (client, resolve, target, info, items, missed, options, time
     return resolve(new Error(`Target tweet is too far a way`));
   }
 
-  missed += options.count - timeline.length;
+  // missed += options.count - timeline.length;
 
-  client.get('/statuses/user_timeline.json', assign({}, options, { max_id: getMaxId(items) }), (err, res, raw)=> {
-    if (err) throw new Error(`Code ${err[0].code}. ${err[0].message}`);
-    setTimeline(client, resolve, target, info, items, missed, options, res);
-  });
+  // client.get('/statuses/user_timeline.json', assign({}, options, { max_id: getMaxId(items) }), (err, res, raw)=> {
+    // if (err) throw new Error(`Code ${err[0].code}. ${err[0].message}`);
+    // setTimeline(client, resolve, target, info, items, missed, options, res);
+  // });
 };
 
-export default(tokens, screen_name, ...args)=> {
-  const resolve = last(args);
-  const target = (args.length === 2) && args[0];
+export default(tokens, screen_name, target, resolve)=> {
   const client = new Twitter(tokens);
-  const options = assign({screen_name}, _options);
+  const options = assign({ screen_name }, _options);
 
-  client.get('/users/show.json', options, (err, info, raw)=> {
+  setTimeline(client, resolve, target, info, [], 0, options, res);
+  client.get('/statuses/user_timeline.json', options, (err, res, raw)=> {
     if (err) throw new Error(`Code ${err[0].code}. ${err[0].message}`);
 
-    if (!target && (info.statuses_count > 3200)) {
-      return resolve(new Error(`@${screen_name} has over the 3200 tweets limit`));
-    }
-
-    client.get('/statuses/user_timeline.json', options, (err, res, raw)=> {
-      if (err) throw new Error(`Code ${err[0].code}. ${err[0].message}`);
-      setTimeline(client, resolve, target, info, [], 0, options, res);
-    });
   });
 };
+*/
