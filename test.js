4837c8787b5f0ff5a5b59681a6d7a08805862683
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-09-23 19:31:36 +0200

    init new test

--- test.js
+++ test.js
@@ -2,32 +2,15 @@ import { equal } from 'assert';
 import getTweets from './';
 import tokens from 'twitter-tokens';
 
-const check = (done)=> (err, tweets, missed, info)=> {
-  equal(tweets.length + missed, info.statuses_count);
-  done();
-};
-
-it('all: should fetch all 37 tweets for @largescalejs_ru', (done)=> {
-  getTweets(tokens, 'largescalejs_ru', check(done));
-});
-
-it('all: should fetch all 1600+ tweets for @andrestaltz', (done)=> {
-  getTweets(tokens, 'andrestaltz', check(done));
-});
-
-it('all: should fetch all 1900+ tweets for @Rygu', (done)=> {
-  getTweets(tokens, 'Rygu', check(done));
-});
-
-it('all: should return an error if account have a lot of tweets', (done)=> {
-  getTweets(tokens, 'jsunderhood', (err, res)=> {
-    equal(err.message, '@jsunderhood has over the 3200 tweets limit')
+it('should return an error if desired tweet is too far a way', (done) => {
+  getTweets(tokens, 'jsunderhood', '562972738303037442', (err, res) => {
+    equal(err.message, 'desired tweet is far a way from 3200 tweets limit')
     done();
   });
 });
 
 it('latest: should return latest tweets incl target one', (done)=> {
-  getTweets(tokens, 'largescaleJS_ru', '424196654758375425', (err, tweets)=> {
+  getTweets(tokens, 'largescaleJS_ru', '424196654758375425', (err, res) => {
     equal('424196654758375425', tweets[tweets.length - 1].id_str);
     done();
   });
