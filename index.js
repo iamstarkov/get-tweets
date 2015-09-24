b262f6fff1add25ab5dc7578e2fb7bb0b82287d4
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-09-24 13:00:56 +0200

    remove alltweets feature

--- index.js
+++ index.js
@@ -1,76 +1,44 @@
 import Twitter from 'twitter';
+import assign  from 'object-assign';
 import bignum  from 'bn.js';
-import { last, concat, contains, propEq, slice, findIndex } from 'ramda';
+import { last, concat, propEq, slice, findIndex, isEmpty } from 'ramda';
 
-function accumulate(client, options, target, cb, tweets) {
+const options = {
+  trim_user: true,
+  count: 200,
+  include_rts: true,
+  exclude_replies: false
+};
+
+function bignumDec(i) {
+  return (new bignum(i).sub(new bignum('1'))).toString();
+}
+
+function getNextTweetsOptions(options, tweets) {
+  if (isEmpty(tweets)) return options;
+  return assign({}, options, { max_id: bignumDec(last(tweets).id_str) });
+}
+
+function accumulate(get, options, target, tweets, cb) {
   const isTarget = propEq('id_str', target);
-  client.get('/statuses/user_timeline.json', options, (err, res) => {
+  const findTargetIndex = findIndex(isTarget);
+  const nextTweetsOptions = getNextTweetsOptions(options, tweets);
+  get(nextTweetsOptions, (err, res) => {
     if (err) throw new err;
-    if (res.length === 0) {
+    if (isEmpty(res)) {
       return cb(new Error('Target tweet is too far a way'));
     }
-    tweets = concat(tweets, res);
-    if (contains(isTarget, tweets)) {
-      return cb(null, slice(0, findIndex(isTarget, tweets) + 1, tweets));
+    const accumulatedTweets = concat(tweets, res);
+    if (findTargetIndex(accumulatedTweets) !== -1) {
+      return cb(null, slice(0, findTargetIndex(accumulatedTweets) + 1, accumulatedTweets));
     }
-    accumulate(client, options, target, cb, tweets);
+    return accumulate(get, options, target, accumulatedTweets, cb);
   });
 }
 
 export default function getTweets(tokens, screen_name, target, cb) {
   const client = new Twitter(tokens);
-  const options = {
-    screen_name,
-    trim_user: true,
-    count: 200,
-    include_rts: true,
-    exclude_replies: false
-  };
-  accumulate(client, options, target, cb, []);
+  const get = client.get.bind(client, '/statuses/user_timeline.json');
+  const optionsWithScreenName = assign({}, { screen_name }, options)
+  return accumulate(get, optionsWithScreenName, target, [], cb);
 };
-
-/*
-const getMaxId = (items) => (new bignum(last(items).id_str).sub(new bignum('1'))).toString();
-const _options = { trim_user: true, count: 200, include_rts: true, exclude_replies: false };
-
-const setTimeline = (client, resolve, target, info, items, missed, options, timeline)=> {
-  client.get('/statuses/user_timeline.json', options, (err, res, raw)=> {
-    if (err) throw new Error(`Code ${err[0].code}. ${err[0].message}`);
-    setTimeline(client, resolve, target, info, [], 0, options, res);
-  });
-
-  items = items.concat(timeline);
-
-
-  // if (!target && (items.length + missed === info.statuses_count)) {
-    // return resolve(undefined, items, missed, info);
-  // }
-
-  if (target && items.find(i => i.id_str === target)) {
-    const number = items.findIndex(i => i.id_str === target) + 1;
-    return resolve(undefined, items.splice(0, number), missed, info);
-  }
-
-  if (target && (items.length + missed > 3200)) {
-    return resolve(new Error(`Target tweet is too far a way`));
-  }
-
-  // missed += options.count - timeline.length;
-
-  // client.get('/statuses/user_timeline.json', assign({}, options, { max_id: getMaxId(items) }), (err, res, raw)=> {
-    // if (err) throw new Error(`Code ${err[0].code}. ${err[0].message}`);
-    // setTimeline(client, resolve, target, info, items, missed, options, res);
-  // });
-};
-
-export default(tokens, screen_name, target, resolve)=> {
-  const client = new Twitter(tokens);
-  const options = assign({ screen_name }, _options);
-
-  setTimeline(client, resolve, target, info, [], 0, options, res);
-  client.get('/statuses/user_timeline.json', options, (err, res, raw)=> {
-    if (err) throw new Error(`Code ${err[0].code}. ${err[0].message}`);
-
-  });
-};
-*/
--- test.js
+++ test.js
@@ -1,17 +1,18 @@
 import { equal } from 'assert';
-import getTweets from './';
+import { last } from 'ramda';
+import getTweets from './index';
 import tokens from 'twitter-tokens';
 
-it('should return an error if desired tweet is too far a way', (done) => {
+it('should return an error if target tweet is too far a way', (done) => {
   getTweets(tokens, 'jsunderhood', '562972738303037442', (err, res) => {
-    equal(err.message, 'desired tweet is far a way from 3200 tweets limit')
+    equal(err.message, 'Target tweet is too far a way')
     done();
   });
 });
 
-it('latest: should return latest tweets incl target one', (done)=> {
-  getTweets(tokens, 'largescaleJS_ru', '424196654758375425', (err, res) => {
-    equal('424196654758375425', tweets[tweets.length - 1].id_str);
+it('latest: should return latest tweets incl target one', (done) => {
+  getTweets(tokens, 'largescalejs_ru', '424196654758375425', (err, res) => {
+    equal('424196654758375425', last(res).id_str);
     done();
   });
 });
